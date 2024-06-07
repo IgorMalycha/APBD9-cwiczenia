8c9068c6461de202024f9c4dbd49f5cf8ce52854
s28192 <s28192@pjwstk.edu.pl>
2024-06-07 15:16:58 +0200

    third endpoit finished, przerpaszam ze tak pozno commit ale myslałem ze termin jest do dzisiaj a nie do wczoraj

--- APBD8-pracadomowaa/APBD8-pracadomowaa/Repositories/TripRepository.cs
+++ APBD8-pracadomowaa/APBD8-pracadomowaa/Repositories/TripRepository.cs
@@ -1,5 +1,6 @@
 ﻿using APBD8_pracadomowaa.Data;
 using APBD8_pracadomowaa.DTOs;
+using APBD8_pracadomowaa.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace APBD8_pracadomowaa.Repositories;
@@ -47,31 +48,42 @@ public class TripRepository : ITripRepository
     
     public async Task AssignClientToTrip(Apbd8Context context, int idTrip, ClientToTripDTO clientToTripDto)
     {
-        
+
+        var idClient = await context.Clients.Where(c => c.Pesel == clientToTripDto.Pesel).Select(c => c.IdClient)
+            .FirstOrDefaultAsync();
+        context.Add(new ClientTrip()
+        {
+            IdTrip = clientToTripDto.IdTrip,
+            IdClient = idClient,
+            PaymentDate = clientToTripDto.PaymentDate,
+            RegisteredAt = DateTime.Now
+        });
+        await context.SaveChangesAsync();
     }
 
     public async Task<bool> ExistsClientWithGivenPesel(Apbd8Context context, ClientToTripDTO clientToTripDto)
     {
-        //return context.Clients.FirstOrDefaultAsync(c => c.Pesel == clientToTripDto.Pesel);
-        return false;
+        return await context.Clients.AnyAsync(c => c.Pesel == clientToTripDto.Pesel);
     }
 
     public async Task<bool> DoesTripOnThisPeselExists(Apbd8Context context, int idTrip, ClientToTripDTO clientToTripDto)
     {
-        //return await _context.ClientTrips.AnyAsync(ct => ct.Client.Pesel == clientToTripDTO.Pesel && ct.IdTrip == idTrip);
-        return false;
+        return await context.ClientTrips.AnyAsync(c => c.IdClientNavigation.Pesel == clientToTripDto.Pesel && c.IdTrip == idTrip);
     }
 
     public async Task<bool> DoesTripExist(Apbd8Context context, int idTrip)
     {
-        
-        return false;
+
+        return await context.Trips.AnyAsync(e => e.IdTrip == idTrip);
     }
 
     public async Task<bool> IsDateFromInPast(Apbd8Context context, ClientToTripDTO clientToTripDto)
     {
-        
-        return false;
+
+        var date = await context.Trips.Where(e => e.IdTrip == clientToTripDto.IdTrip)
+            .Select(e => e.DateFrom).FirstOrDefaultAsync();
+
+        return date > DateTime.Now;
     }
 
 }
